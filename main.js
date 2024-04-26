c735258dacd3020a9afd6f89f806d1b3d6fb543f
Paraldos <hillbrecht.s@gmail.com>
2024-04-26 08:36:03 +0200

    removed nachteile

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import Attribute from "./js/attribute.js";
 import Fertigkeiten from "./js/fertigkeiten.js";
 import Talente from "./js/talente.js";
-import Nachteile from "./js/nachteile.js";
 import Merkmale from "./js/merkmale.js";
 import Inventar from "./js/inventar.js";
 
