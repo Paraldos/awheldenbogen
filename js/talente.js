6f99eac185bd04dacf5e53b74fc957c552c964cc
Paraldos <hillbrecht.s@gmail.com>
2024-04-26 08:35:33 +0200

    reworked Talente

--- js/talente.js
+++ js/talente.js
@@ -1,16 +1,21 @@
 import Nodetype from "./nodetype.js";
 
 export default class Talente extends Nodetype {
-    constructor() {
-        super()
-        this.talente = document.querySelector('.talente')
-        this.talente.innerHTML = `
-            ${this.createHeader('Talente')}
+  constructor() {
+    super();
+    this.talente = document.querySelector(".talente");
+    this.talente.innerHTML = `
+            ${this.createHeader("Talente / Nachteile")}
             <div class='talente__container'>
                 <div></div>
                 <div></div>
                 <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
-        `
-    }
+        `;
+  }
 }
--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-import Attribute from "./js/attribute.js"
-import Fertigkeiten from "./js/fertigkeiten.js"
-import Talente from "./js/talente.js"
-import Nachteile from "./js/nachteile.js"
-import Merkmale from "./js/merkmale.js"
-import Inventar from "./js/inventar.js"
+import Attribute from "./js/attribute.js";
+import Fertigkeiten from "./js/fertigkeiten.js";
+import Talente from "./js/talente.js";
+import Nachteile from "./js/nachteile.js";
+import Merkmale from "./js/merkmale.js";
+import Inventar from "./js/inventar.js";
 
-new Attribute()
-new Fertigkeiten()
-new Talente()
-new Nachteile()
-new Merkmale()
-new Inventar()
+new Attribute();
+new Fertigkeiten();
+new Talente();
+new Merkmale();
+new Inventar();
