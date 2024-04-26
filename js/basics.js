3bf16d00a51e1f1a4dcb1bb15edb4a05d8ab6f01
Paraldos <hillbrecht.s@gmail.com>
2024-04-26 08:55:17 +0200

    added and styled basics

--- js/basics.js
+++ js/basics.js
@@ -6,6 +6,19 @@ export default class Basics extends Nodetype {
     this.basics = document.querySelector(".basics");
     this.basics.innerHTML = `
         ${this.createHeader("Heldenbogen", "h1")}
+        <div class='basics__content'>
+            <div></div>
+            <div></div>
+            <label>Name</label>
+            <label>Rasse</label>
+            <div></div>
+            <div></div>
+            <label>Klasse</label>
+            <label>Motive</label>
+        </div>
+
+        <div class='basics__description'></div>
+        <label>Beschreibung</label>
     `;
   }
 }
